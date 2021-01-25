66e97dc5033135f4c71d0084af03efef26cdf384
ermanergoz <erman.ergoz@gmail.com>
2021-01-25 22:49:38 +0100

    Clean up

--- app/src/main/java/com/erman/beeper/BatteryOptimizationDialog.kt
+++ app/src/main/java/com/erman/beeper/BatteryOptimizationDialog.kt
@@ -3,7 +3,6 @@ package com.erman.beeper
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 
